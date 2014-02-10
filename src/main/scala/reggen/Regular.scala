aa9e07a171110b870715170fc36f2cd736b25a16
gclaramunt <gabriel.claramunt@gmail.com>
2014-02-10 00:23:22 -0200

    move to non parametric regular generics (functors instead of bifunctors)

--- src/main/scala/reggen/Regular.scala
+++ src/main/scala/reggen/Regular.scala
@@ -1,449 +1,222 @@
 package reggen
 
 import scala.language.higherKinds
+import scala.language.existentials //double check 
 
+//non-parametric regular types
 object Regular {
 
-/*
-  -- | **********************************
-  -- | ** Tipos regulares parametricos **
-  -- | **********************************
+  /*
+-- | *******************
+-- | ** Class Regular **
+-- | *******************
 
-  -- | Como visto en el curso, los tipos regulares parametricos pueden ser
-  -- | representados en funcion de bifunctores regulares.
-  -- | Para ello es necesario definir una nueva clase, que llamaremos
-  -- | Regular2 y una nueva familia de tipos, PF2, que asocia un bifunctor
-  -- | (representado por un constructor de tipo de kind * -> * -> *) a
-  -- | a cada constructor t de kind * -> * que es declarado instancia de
-  -- | la familia de tipos.
+-- | Primeramente veamos la definicion de los functores regulares
+-- | y su correspondiente instancia de la clase Functor.
+-- |
+-- | class Functor f where
+-- |   fmap :: (a -> b) -> (f a -> f b)
 */
-  // type family PF2 (t :: * -> *) :: * -> * -> *
-  // ej type instance PF2 List = U2 :++: (Par :**: Rec)
+
+trait Functor[F[_]] {
+  def fmap[A, B](fa: F[A])(f: A => B): F[B]
+}
+
+
+
 /*
-  //NO ENCODING ????
-  trait PF2[TT[_],RR[_,_]]
 
-  implicit def pf2List = new PF2[List]{ type RR= :++:[U2,Par:**:Rec]}
+infixr 6 :+:
+infixr 7 :*:
 
-  // class Regular2 t where
-  //    from2 :: t a -> (PF2 t) a (t a)  --- recibe t a y devuelve el tipo resultante de aplicar a PF2(t) a y t a
-  //    to2   :: (PF2 t) a (t a) ->  t a
+-- | Functor constante
 
-  trait Regular2[T[_]]{
-    def from2[A]:T[A]=>PF2[A,T] 
-    def to2[A]:PF2[A,T]=>T[A]
+newtype K a r = K { unK :: a }
+
+instance Functor (K a) where
+  fmap _ (K a) = K a
+*/
+
+case class K[A,R](unK:A) 
+
+implicit def FK[A] =new Functor[({type λ[B]=K[A,B]})#λ]{
+  def fmap[A1, B1](ka: K[A,A1])(f: A1 => B1): K[A,B1]=K(ka.unK)
+}
+
+/*
+-- | Functor constante 1 (equivale a K ())
+
+data U r = U
+
+instance Functor U where
+  fmap _ U = U
+*/
+case class U[R]() 
+
+implicit def FU =new Functor[U]{
+  def fmap[A, B](ua: U[A])(f: A => B):U[B]=U()
+}
+
+/*
+
+
+-- | Functor identidad
+
+newtype I r = I { unI :: r }
+
+instance Functor I where
+  fmap f (I r) = I (f r)
+
+*/
+case class Id[T](unI:T)
+implicit def FId =new Functor[Id]{
+  def fmap[A, B](ida: Id[A])(f: A => B):Id[B]=Id(f(ida.unI))
+}
+
+/*
+
+-- | Suma de functores
+
+data (f :+: g) r = L (f r) | R (g r)
+
+instance (Functor f, Functor g) => Functor (f :+: g) where
+  fmap f (L x) = L (fmap f x)
+  fmap f (R y) = R (fmap f y)
+
+*/
+
+trait :+:[F,G]
+
+case class L[F,G](f:F) extends :+:[F,G]
+case class R[F,G](g:G) extends :+:[F,G]
+
+trait FunctorCoprod[F[_],G[_]]{
+  type abs[A]= F[A]:+:G[A]
+}
+
+implicit def fplus[A,F[_]:Functor, G[_]:Functor](implicit ff:Functor[F], fg:Functor[G])=new Functor[FunctorCoprod[F,G]#abs]{
+  def fmap[A,B](fga:F[A]:+:G[A])(f: A => B):F[B]:+:G[B]= fga match {
+    case L(lf) => L(ff.fmap(lf)(f))
+    case R(rg) => R(fg.fmap(rg)(f))
   }
-*/
+}
 
+
+/*
+-- : Producto de functores
+
+data (f :*: g) r = f r :*: g r
+
+instance (Functor f, Functor g) => Functor (f :*: g) where
+  fmap f (x :*: y) = fmap f x :*: fmap f y
+
+*/
+case class :*:[F,G](f:F,g:G)
+
+trait FunctorProd[F[_],G[_]]{
+  type abs[A]= F[A]:*:G[A]
+}
+
+implicit def fstar[A,F[_]:Functor, G[_]:Functor](implicit ff:Functor[F], fg:Functor[G])=new Functor[FunctorProd[F,G]#abs]{
+  def fmap[A,B](fga:F[A]:*:G[A])(f: A => B):F[B]:*:G[B]= :*:(ff.fmap(fga.f)(f),fg.fmap(fga.g)(f))
+}
+
+/*
+-- | Composicion de functores
+
+newtype (f :@: g) r = Comp {unComp :: f (g r)}
+
+instance (Functor f, Functor g) => Functor (f :@: g) where
+  fmap f r = Comp $ fmap (fmap f) $ unComp r
+*/
+case class |@|[F,G](f:F,g:G)
+
+trait FunctorComp[F[_],G[_]]{
+  type abs[A]= F[A]|@|G[A]
+}
+
+implicit def fstar[A,F[_]:Functor, G[_]:Functor](implicit ff:Functor[F], fg:Functor[G])=new Functor[FunctorComp[F,G]#abs]{
+  def fmap[A,B](fga:F[A]|@|G[A])(f: A => B):F[B]|@|G[B]= |@|(ff.fmap(fga.f)(f),fg.fmap(fga.g)(f))
+}
+
+/*
+
+-- | Dado un tipo de dato T con functor base F, su representacion en terminos
+-- | de la clase Regular es a traves de dos funciones from :: T -> F T y
+-- | to :: F T -> T, que son las funciones testigos del isomorfismo T ~ F T.
+
+-- | La representacion del functor base de un tipo de dato (tambien llamado
+-- | "pattern functor") se hace a traves de lo que en Haskell se denomina
+-- | una familia de tipos (algo asi como la definicion de una "funcion" a
+-- | nivel de tipos). Dado un tipo "a", PF retorna un constructor de tipo
+-- | de kind * -> * el cual correponde a la representaion Haskell del functor
+-- | del tipo "a".
+
+type family PF a :: * -> *
+
+class Regular a where
+   from :: a -> (PF a) a
+   to   :: (PF a) a -> a
+
+-- | Ejemplo: Veamos la definicion del tipo TreeInt (arboles binarios de enteros)
+-- | en terminos de la clase Regular.
+
+data TreeInt = LeafI Int | NodeI TreeInt TreeInt
+  deriving Show
+
+-- | La definicion del functor base de TreeInt se hace mediante la introduccion
+-- | de una instancia de la familia PF.
+-- | Corresponde al functor: F A = Int + A x A.
+
+type instance PF TreeInt = K Int :+: (I :*: I)
+
+-- | Definicion de la instancia de Regular para TreeInt.
+
+instance Regular TreeInt where
+  from (LeafI n)   = L (K n)
+  from (NodeI x y) = R (I x :*: I y)
+
+  to (L (K n))         = LeafI n
+  to (R (I x :*: I y)) = NodeI x y
+
+-- | Tipos parametricos tambien se pueden representar mediante la clase Regular.
+-- | Como se vio en el curso, esto se hace usando un functor parametrizado.
+-- | Veamos como ejemplo el tipo "List a" de las listas de tipo a.
+
+data List a = Nil | Cons a (List a)
+  deriving Show
+
+-- | Definicion del functor base. Corresponde al functor L_A B = 1 + A x B.
+
+type instance PF (List a) = U :+: (K a :*: I)
+
+instance Regular (List a) where
+  from Nil         = L U
+  from (Cons x xs) = R (K x :*: I xs)
+
+  to (L U)              = Nil
+  to (R (K x :*: I xs)) = Cons x xs
+
+-- | Definicion generica del operador fold.
+-- | Esta definicion es valida para todo tipo "d" que sea instancia de Regular
+-- | y cuyo functor "PF d" sea instancia de Functor.
+
+fold  :: (Regular d, Functor (PF d)) => (PF d a -> a) -> d -> a
+fold h = h . fmap (fold h) . from
+
+-- | Ejemplos
+
+sumTreeInt :: TreeInt -> Int
+sumTreeInt = fold h
+  where
+    h (L (K n))         = n
+    h (R (I x :*: I y)) = x + y
+
+sumList :: Num a => List a -> a
+sumList = fold h
+  where
+    h (L U)             = 0
+    h (R (K n :*: I m)) = n + m
+  */
   
-
-  /*
-    -- | Bifunctores regulares. Al igual que para los (mono)functores regulares
-    -- | definiremos los bifunctores regulares en Haskell y sus correspondientes
-    -- | instancias de la clase Bifunctor.
-
-    class Bifunctor f where
-       bimap :: (a -> b) -> (r -> s) -> f a r -> f b s
-  */
-/*  trait Bifunctor[F[_, _]] {
-    def bimap[A,R,B,S](f: A => B, g: R => S): F[A, R]=>F[B, S]
-  }
-*/
-  trait Bifunctor[F[_, _]] {
-    def bimap[A, R, B, S](fa: F[A, R], f: A => B, g: R => S): F[B, S]
-  }
-
-
-  /*
-      infixr 6 :++:
-      infixr 7 :**:
-
-    no way :(
-  */
-
-  /*
-    -- | Bifunctor constante
-
-    data K2 a b r = K2 a
-  */
-  case class K2[A,B,R](a:A) 
-
-  /*
-    instance Bifunctor (K2 a)  where
-       bimap f g (K2 x) = (K2 x)
-  */
-  implicit def Bik2[A] =new Bifunctor[ ({type λ[B,R]=K2[A,B,R]})#λ ]{
-    def bimap[A1, R, B, S](k2:K2[A,A1,R], f: A1 => B,g: R => S)= K2[A,B,S](k2.a) 
-  }
-
-  /*
-    -- | Bifunctor constante 1
-
-    data U2 a r = U2
-  */
-  case class U2[A,R]()
-  /*
-    instance Bifunctor U2  where
-       bimap f g U2 = U2
-  */
-  implicit def BiU2 =new Bifunctor[U2]{
-    def bimap[A, R, B, S](u2:U2[A,R], f: A => B,g: R => S)= U2[B,S]()
-  }
-
-  /*
-    -- | Primera proyeccion (ocurrencia parametro)
-
-    data Par a r = Par a
-  */
-  case class Par[A,R](a:A)
-  /*
-    instance Bifunctor Par where
-       bimap f g (Par x) = Par (f x)
-  */
-  implicit def BiPar= new Bifunctor[Par]{
-    def bimap[A, R, B, S]( par:Par[A,R], f: A => B,g: R => S)=  Par[B,S](f(par.a))
-  }
-
-  /*
-    -- | Segunda proyeccion (ocurrencia posicion recursiva)
-
-    data Rec a r = Rec r
-  */
-  case class Rec[A,R](r:R)
-  /*
-    instance Bifunctor Rec where
-       bimap f g (Rec r) = Rec (g r)
-  */
-  implicit def BiRec = new  Bifunctor[Rec] {
-    def bimap[A, R, B, S](rec:Rec[A,R], f: A => B,g: R => S)=  Rec[B,S](g(rec.r))
-  }
-
-  /*
-    -- | Suma de bifunctores
-
-    data (f :++: g) a r = LL (f a r) | RR (g a r)
-  */
-
-  //ugh! must be a better way
-  trait :++:[A,R,F[_,_],G[_,_]]
-  case class LL[A,R,F[_,_],G[_,_]](f:F[A,R]) extends :++:[A,R,F,G]
-  case class RR[A,R,F[_,_],G[_,_]](g:G[A,R]) extends :++:[A,R,F,G]
-
-    /*
-      instance (Bifunctor t, Bifunctor u) => Bifunctor (t :++: u) where
-         bimap f g (LL x) = LL (bimap f g x)
-         bimap f g (RR y) = RR (bimap f g y)
-    */
-
-  implicit def BiPlus[F[_,_],G[_,_]](implicit bf:Bifunctor[F],bg:Bifunctor[G])=new Bifunctor[({type λ[A,R]= :++:[A,R,F,G]})#λ] {
-    def bimap[A, R, B, S](fa: :++:[A,R,F,G], f: A => B, g: R => S): :++:[B,S,F,G]=fa match {
-      case LL(lf) =>LL(bf.bimap(lf,f,g))
-      case RR(rg) =>RR(bg.bimap(rg,f,g))
-    }
-  }
-
-
-  /*
-    -- | Producto de bifunctores
-
-    data (f :**: g) a r = f a r :**: g a r
-  */
-
-  //:**:
-  case class **[A,R,F[_,_],G[_,_]](f:F[A,R],g:G[A,R])
-
-  /*
-  instance (Bifunctor t, Bifunctor u) => Bifunctor (t :**: u) where
-     bimap f g (x :**: y) = bimap f g x :**: bimap f g y
-  */
-  implicit def BiStar[F[_,_],G[_,_]](implicit bf:Bifunctor[F],bg:Bifunctor[G])=new Bifunctor[({type λ[A,R]= **[A,R,F,G]})#λ] {
-    def bimap[A, R, B, S](fa: **[A,R,F,G], f: A => B, g: R => S): **[B,S,F,G]= **(bf.bimap(fa.f,f,g),bg.bimap(fa.g,f,g))
-  }
-
-
-  // type family PF2 (t :: * -> *) :: * -> * -> *
-  // ej type instance PF2 List = U2 :++: (Par :**: Rec)
-  /*
-    //NO ENCODING ????
-    trait PF2[TT[_],RR[_,_]]
-
-    implicit def pf2List = new PF2[List]{ type RR= :++:[U2,Par:**:Rec]}
-
-    // class Regular2 t where
-    //    from2 :: t a -> (PF2 t) a (t a)  --- recibe t a y devuelve el tipo resultante de aplicar a PF2(t) a y t a
-    //    to2   :: (PF2 t) a (t a) ->  t a
-
-  */
-  trait PF2[T[_,_]] {
-    type INST
-  }
-
-  trait Regular2[T[_]]{
-    def from2[A,PF2[_,_]]:T[A]=>PF2[A,T[A]] 
-    def to2[A,PF2[_,_]]:PF2[A,T[A]]=>T[A]
-  }
-
-
-  type PARREC[A]= **[A,List[A],Par[A,List[A]],Rec[A,List[A]]]
-  type BF_LIST[A]=U2:++:PARREC[A,List]
-  implicit def pf2OfList = new PF2[List]{ type INST= BF_LIST}
-  //implicit def regOfList= new 
-
-  //trait PF2[TT[_],RR[_,_]]
-  /*
-  trait PF2[T[_]]{
-    type BF[_,_]
-
-  }
-*/
-  /*
-  -- | Functor de tipo compuesto con bifunctor
-
-  data (d :@@: f) a r = Comp2 {unComp2 :: d (f a r)}
-  */
-  case class Comp2[A,R,F[A,R],D[F[A,R]]](unComp2:D[F]){
-    type PF2D[X]=PF2[X,D[X]]
-  }
-  type :@@:[A,R,F[A,R],D[F[A,R]]] = Comp2[A,R,F,D]
-
-/*
-  trait CompApply[D[_],F[_,_],PF2[_,_]]{
-    type PF2D[X]=PF2[X,D[X]]
-    type BF[A,R]=Comp2[A,R,F,PF2D]
-  }*/
-
-  /*
-  -- | *********************************************
-  -- | ** Definicion generica del functor de tipo **
-  -- | *********************************************
-
-  pmap :: (Regular2 d, Bifunctor (PF2 d)) => (a -> b) -> d a -> d b
-  pmap f = to2 . bimap f (pmap f) . from2 
-        == to2(bimap f (pmap f) from2 da )  
-*/
-  def pmap[A,B,D[_]](da:D[A],f:A=>B)(implicit d:Regular2[D], pf2:PF2[D], bf:Bifunctor[DF2D]):D[B]={
-    //bimap :: (a -> b) -> (r -> s) -> f a r -> f b s
-    d.to2(bf.bimap(d.from2(da),f, (x:D[A])=>pmap(x,f)(d,bf)))
-  }
-
-  /*
-  instance (Regular2 d, Bifunctor (PF2 d), Bifunctor f) => Bifunctor (d :@@: f) where
-     bimap f g x = Comp2 $ pmap (bimap f g) $ unComp2 x
-  */
-
-  implicit def BiComp[F[_,_]:Bifunctor,D[_]:Regular2,PF2[_,_]:Bifunctor]=new Bifunctor[CompApply[D,F,PF2]#BF]{
-    
-    val bf=implicitly[Bifunctor[F]]
-    val d=implicitly[Regular2[D]]
-    def bimap[A, R, B, S](fa: Comp2[A,R,F,PF2D], f: A => B, g: R => S): Comp2[B,S,F,PF2D]= Comp2(
-      pmap(fa.unComp2, (x:F[A,R]) => bf.bimap(x,f,g))(d,bf)
-    )
-  }
-
-/*
- trait Bifunctor[F[_, _]] {
-    def bimap[A, R, B, S](fa: F[A, R], f: A => B, g: R => S): F[B, S]
-  }
-*/
-
-
-/*
-  -- | *******************************************
-  -- | ** Definicion generica del operador fold **
-  -- | *******************************************
-
-  fold2  :: (Regular2 d, Bifunctor (PF2 d)) => (PF2 d a b -> b) -> d a -> b
-  fold2 h = h . bimap id (fold2 h) . from2
-*/
-  def fold2[A,B,D[A]:Regular2,PF2D[A,B]:Bifunctor](da:D[A],h:PF2D[A,B]=>B):B={
-    val bf=implicitly[Bifunctor[PF2D]]
-    val d=implicitly[Regular2[D]]
-    h(bf.bimap(d.from2(da), identity ,((x:D[A])=>fold2(x,h)(d,bf))))
-  }
-
-
-
-/*
-  -- Alternativamente:
-  -- pmap f = fold2 (to2 . bimap f id)
-
-  -- | Ejemplo: List
-  -- | data List a = Nil | Cons a (List a)
-
-  type instance PF2 List = U2 :++: (Par :**: Rec)
-
-  instance Regular2 List where
-    from2 Nil         = LL U2
-    from2 (Cons x xs) = RR (Par x :**: Rec xs)
-
-    to2 (LL U2)                  = Nil
-    to2 (RR (Par x :**: Rec xs)) = Cons x xs
-
-  sumList2 :: Num a => List a -> a
-  sumList2 = fold2 h
-    where
-      h (LL U2)                 = 0
-      h (RR (Par n :**: Rec m)) = n + m
-
-  -- | Ejemplo: Arboles binarios con informacion en las hojas
-
-  data Btree a = Leaf a | Join (Btree a) (Btree a) deriving Show
-
-  type instance PF2 Btree = Par :++: (Rec :**: Rec)
-
-  instance Regular2 Btree where
-    from2 (Leaf a)   = LL (Par a)
-    from2 (Join t u) = RR (Rec t :**: Rec u)
-
-    to2 (LL (Par a))            = Leaf a
-    to2 (RR (Rec t :**: Rec u)) = Join t u
-
-  sumBtree :: Num a => Btree a -> a
-  sumBtree = fold2 h
-    where
-      h (LL (Par a))            = a
-      h (RR (Rec m :**: Rec n)) = m + n
-
-  -- | Ejemplo: Rose trees
-
-  data Rose a = Fork a (List (Rose a))
-
-  type instance PF2 Rose = Par :**: (List :@@: Rec)
-
-  instance Regular2 Rose where
-    from2 (Fork a rs) = Par a :**: (Comp2 (pmap Rec rs))
-
-    to2 (Par a :**: Comp2 rs) = Fork a (pmap unRec rs)
-      where
-        unRec (Rec r) = r
-
-  sumRose :: Num a => Rose a -> a
-  sumRose = fold2 h
-    where
-      h (Par a :**: Comp2 rs) = a + sumList2 (pmap unRec rs)
-      unRec (Rec r) = r
-
-  -- | **************************************
-  -- | ** Definicion suma politipica: psum **
-  -- | **************************************
-
-  -- | La siguiente definicion de psum funciona para toda estructura "d"
-  -- | que sea definida como instancia de la clase Regular2 y cuyo functor
-  -- | "PF2 d" sea instancia de Bifunctor (este es un requerimiento de fold2).
-  -- | Para que la definicion termine siendo politipica, el algebra fsum del fold
-  -- | se define como el unico metodo de una clase que llamamos C_fsum. Luego,
-  -- | se definen instancias de esta clase para cada bifunctor regular.
-  -- | En el contexto de tipos de psum se debe agregar la restriccion de que
-  -- | hay una instancia de C_fsum para el bifunctor "PF2 d" del tipo de dato d.
-
-  -- | Esta es la "metodologia" que usaremos para definir funciones politipicas
-  -- | asociadas a tipos de datos representables por Regular2. Es muy similar a
-  -- | lo que internamente define PolyP 2.
-
-  psum :: (Regular2 d, Bifunctor (PF2 d), C_fsum (PF2 d), Num a) => d a -> a
-  psum = fold2 fsum
-
-  class C_fsum f where
-    fsum :: Num a => f a a -> a
-
-  instance C_fsum (K2 a) where
-    fsum _ = 0
-
-  instance C_fsum U2 where
-    fsum _ = 0
-
-  instance C_fsum Par where
-    fsum (Par a) = a
-
-  instance C_fsum Rec where
-    fsum (Rec r) = r
-
-  instance (C_fsum f, C_fsum g) => C_fsum (f :++: g) where
-    fsum (LL x) = fsum x
-    fsum (RR y) = fsum y
-
-  instance (C_fsum f, C_fsum g) => C_fsum (f :**: g) where
-    fsum (x :**: y) = fsum x + fsum y
-
-  -- | Las restricciones Regular2 d, Bifunctor (PF2 d), C_fsum (PF2 d) son
-  -- | necesarias para poder invocar a psum sobre el tipo d.
-  instance (Regular2 d, Bifunctor (PF2 d), C_fsum (PF2 d), C_fsum f)
-           => C_fsum (d :@@: f) where
-    fsum (Comp2 x) = psum . pmap fsum $ x
-
-  -- | Ejemplos de llamadas
-
-  lista = Cons 3 (Cons 4 Nil)
-  arbol = Join (Leaf 5) (Leaf 6)
-  rose  = Fork 4 (Cons (Fork 5 Nil) (Cons (Fork 6 Nil) Nil))
-
-  psum1 = psum $ lista
-  psum2 = psum $ arbol
-  psum3 = psum $ rose
-
-  -- | ***********************************
-  -- | ** Definicion flatten politipico **
-  -- | ***********************************
-
-  flatten :: (Regular2 d, Bifunctor (PF2 d), C_fl (PF2 d)) => d a -> [a]
-  flatten = fold2 fl
-
-  class C_fl f where
-    fl :: f a [a] -> [a]
-
-  instance C_fl (K2 a) where
-    fl _ = []
-
-  instance C_fl U2 where
-    fl _ = []
-
-  instance C_fl Par where
-    fl (Par a) = [a]
-
-  instance C_fl Rec where
-    fl (Rec r) = r
-
-  instance (C_fl f, C_fl g) => C_fl (f :++: g) where
-    fl (LL x) = fl x
-    fl (RR y) = fl y
-
-  instance (C_fl f, C_fl g) => C_fl (f :**: g) where
-    fl (x :**: y) = fl x ++ fl y
-
-  instance (Regular2 d, Bifunctor (PF2 d), C_fl (PF2 d), C_fl f)
-           => C_fl (d :@@: f) where
-    fl (Comp2 x) = concat . flatten . pmap fl $ x
-
-  -- | ***************************************
-  -- | ** Definicion size politipico: psize **
-  -- | ***************************************
-
-  psize :: (Regular2 d, Bifunctor (PF2 d), C_fsize (PF2 d)) => d a -> Int
-  psize = fold2 fsize
-
-  class C_fsize f where
-    fsize :: f a Int -> Int
-
-  instance C_fsize (K2 a) where
-    fsize _ = 0
-
-  instance C_fsize U2 where
-    fsize _ = 0
-
-  instance C_fsize Par where
-    fsize (Par a) = 1
-
-  instance C_fsize Rec where
-    fsize (Rec r) = r
-
-  instance (C_fsize f, C_fsize g) => C_fsize (f :++: g) where
-    fsize (LL x) = fsize x
-    fsize (RR y) = fsize y
-
-  instance (C_fsize f, C_fsize g) => C_fsize (f :**: g) where
-    fsize (x :**: y) = fsize x + fsize y
-
-  instance (Regular2 d, Bifunctor (PF2 d), C_fsum (PF2 d), C_fsize f)
-           => C_fsize (d :@@: f) where
-    fsize (Comp2 x) = psum . pmap fsize $ x*/
 }
