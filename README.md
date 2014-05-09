e322d7155634b45649957c71b03b68697cd242ee
gclaramunt <gabriel.claramunt@gmail.com>
2014-05-09 14:56:48 -0400

    flatten example more documentation work

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ scala-reggen
 
 Generic programming experiment in Scala
 
-A category theory based approach to generic programming, based on a pattern functor description of the type and defining a behavior for each regular functor class.
+A category theory based approach to generic programming, where each datatype is described by a functor polynomial and defining a function used to fold each regular functor class.
 
 e.g.
 
@@ -11,4 +11,52 @@ List\[A\] ( Nil | Cons (a:A, l:List\[A\]) ) is represented as 1 + K A * I
 
 Tree\[A\] (Leaf(a:A) |  Node(l:Tree\[A\],r:Tree\[A\])) is represented as K A + I * I
 
+
+
 In homage to Miles Sabin's awesome [Shapeless](https://github.com/milessabin/shapeless) this library should be eventually called "Useless"
+
+Background
+----------
+
+### A 100 mph tour into Category theory land
+
+This library uses category theory as the basis for its machinery, so to understand how it works, we need some theory.
+
+[Alissa Pajer](http://alissapajer.github.io/conferenceslides/craftconf2014/#/) does a way better job than me
+
+### Categories
+A category is a collection of objects and arrows between them with identity and composition.
+More formally:
+
+Obj(Cat): collection of objects.
+For each A,B ∈ Obj(Cat), there’s a set C(A,B) of morphisms (arrows) from A to B
+f:A→B means f ∈ C(A,B)
+
+Composition operation between arrows:
+if f:A→B and g:B→C, then g∘f:A→C
+Must be associative: f∘(g∘h) = (f∘g)∘h
+
+For each object X, exists an identity arrow IdX:X→X, such as IdB∘f=f∘IdA=f for each f:A→B
+
+In other words, to effectively form a Category, the choosen arrow composition must be associative and itmust be a unique 
+
+### Functors
+A functor from cat. C to cat. D is a pair of functions one maps objects of C to D and the other maps arrows of C to D
+functor F:C→D, A ∈C⇒FA∈D, f ∈C⇒Ff∈D
+
+A functor must satisfy the following laws:
+Ff:FA→FB
+F(f∘g)=Ff∘Fg
+FidA=idFA
+
+### Folds
+
+(define fold?)
+  (laws)
+
+
+(TO BE DONE:)
+(how types and functions are a category)
+(how type constructors )
+(how types can be described as an algebra of functors)
+(how fold is the unversal operation)
--- src/main/scala/reggen/SampleGenericCode.scala
+++ src/main/scala/reggen/SampleGenericCode.scala
@@ -17,9 +17,12 @@ object SampleGenericCode extends App {
     case l:L[Regular[Z]#PF[Int],_] @unchecked => sum(l.f)
     case r:R[_,Regular[Z]#PF[Int]] @unchecked => sum(r.g)
     case star:(Regular[Z]#PF[Int]:*:Regular[Z]#PF[Int]) @unchecked => sum(star.f)+sum(star.g)
-    case c:Comp[_,_,_] => 0 // ??
   }
- 
+  println("sum of TreeInt = " + fold(ti)(sum))
+  println("sum of Tree[Int] = " + fold(tp)(sum))
+  println("sum of List[Int] = " + fold(l)(sum))
+  println
+
   def count[Z]:Regular[Z]#PF[_]=>Int = {
       case U() => 0
       case k:K[_,Z] @unchecked=> 1
@@ -27,9 +30,13 @@ object SampleGenericCode extends App {
       case l:L[Regular[Z]#PF[_],_] @unchecked => count(l.f)
       case r:R[_,Regular[Z]#PF[_]] @unchecked => count(r.g)
       case star:(Regular[Z]#PF[_]:*:Regular[Z]#PF[_]) @unchecked => count(star.f)+count(star.g)
-      case c:Comp[_,_,_] => 0 // ??
+      //case c:Comp[_,_,_] => 0 
     }
-  
+  println("count of TreeInt = " + fold(ti)(count))
+  println("count of Tree[Int] = " + fold(tp)(count))
+  println("count of List[Int] = " + fold(l)(count))
+  println
+
   def max[Z]:Regular[Z]#PF[Int]=>Int = {
       case U() => 0
       case k:K[Int,Z] @unchecked=> k.unK
@@ -37,18 +44,37 @@ object SampleGenericCode extends App {
       case l:L[Regular[Z]#PF[Int],_] @unchecked => max(l.f)
       case r:R[_,Regular[Z]#PF[Int]] @unchecked => max(r.g)
       case star:(Regular[Z]#PF[Int]:*:Regular[Z]#PF[Int]) @unchecked => Math.max(max(star.f),max(star.g))
-      case c:Comp[_,_,_] => 0 // ??
+      //case c:Comp[_,_,_] => 0 
     }
-
-  println("sum of TreeInt = " + fold(ti)(sum))
-  println("sum of Tree[Int] = " + fold(tp)(sum))
-  println("sum of List[Int] = " + fold(l)(sum))
-
-  println("count of TreeInt = " + fold(ti)(count))
-  println("count of Tree[Int] = " + fold(tp)(count))
-  println("count of List[Int] = " + fold(l)(count))
-
   println("max of TreeInt = " + fold(ti)(max))
   println("max of Tree[Int] = " + fold(tp)(max))
   println("max of List[Int] = " + fold(l)(max))
+  println
+
+  def flatten[Z]:Regular[Z]#PF[_]=>List[_] = {
+      case U() => Nil
+      case k:K[_,Z] @unchecked=> List(k.unK)
+      case i:I[List[_]] @unchecked=> i.unI 
+      case l:L[Regular[Z]#PF[_],_] @unchecked =>  flatten(l.f)
+      case r:R[_,Regular[Z]#PF[_]] @unchecked => flatten(r.g)
+      case star:(Regular[Z]#PF[_]:*:Regular[Z]#PF[_]) @unchecked => flatten(star.f)++flatten(star.g)
+      //case c:Comp[_,_,_] => Nil 
+    }
+  println("flatten of TreeInt = " + fold(ti)(flatten))
+  println("flatten of Tree[Int] = " + fold(tp)(flatten))
+  println("flatten of List[Int] = " + fold(l)(flatten))
+  println
+
+  def serialize[Z]:Regular[Z]#PF[_]=>String = {
+      case U() => "nil"
+      case k:K[_,Z] @unchecked=> k.unK.toString
+      case i:I[String] @unchecked=> i.unI
+      case l:L[Regular[Z]#PF[Int],_] @unchecked => serialize(l.f)
+      case r:R[_,Regular[Z]#PF[Int]] @unchecked => serialize(r.g)
+      case star:(Regular[Z]#PF[Int]:*:Regular[Z]#PF[Int]) @unchecked => serialize(star.f)+","+serialize(star.g)
+    }
+  println("serialize of TreeInt = " + fold(ti)(serialize))
+  println("serialize of Tree[Int] = " + fold(tp)(serialize))
+  println("serialize of List[Int] = " + fold(l)(serialize))
+  
 }
