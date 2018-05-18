b4fa041fffafc69a59d7ece3c016c9086d8af670
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2018-05-18 00:42:05 -0300

    lipstick

--- src/main/scala/reggen/Bifunctors.scala
+++ src/main/scala/reggen/Bifunctors.scala
@@ -1,8 +1,6 @@
 package reggen
 
-import Bifunctors._
-import Functors._
-import Regular2._
+import reggen.Regular2._
 
 /*
 -- | Bifunctores regulares. Al igual que para los (mono)functores regulares
@@ -14,15 +12,15 @@ class Bifunctor f where
 
    infixr 6 :++:
    infixr 7 :**:
-*/
+ */
 
-trait Bifunctor[F[_,_]] {
-  def bimap[A, B, R, S](fa: F[A,R])(f: A => B,g:R=>S): F[B,S]
+trait Bifunctor[F[_, _]] {
+  def bimap[A, B, T, S](fa: F[A, T])(f: A => B, g: T => S): F[B, S]
 }
 
 //combines two bifunctors with a bifunctor using the same two parameters
-trait BiFComb[F[_,_],G[_,_],K[_,_]]{
-  type abs[A,B]= K[F[A,B],G[A,B]]
+trait BiFComb[F[_, _], G[_, _], H[_, _]] {
+  type abs[A, B] = H[F[A, B], G[A, B]]
 }
 
 object Bifunctors {
@@ -34,30 +32,32 @@ object Bifunctors {
 
    instance Bifunctor (K2 a)  where
       bimap f g (K2 x) = (K2 x)
-  */
+   */
 
-  case class K2[A,B,R](unK:A) 
+  case class K2[A, B, T](unK: A)
 
-  implicit def FK2[A] =new Bifunctor[K2[A,?,?]]{
-    def bimap[A1,B1,A2,B2](ka: K2[A,A1,A2])(f: A1 => B1, g: A2 => B2): K2[A,B1,B2]=K2(ka.unK)
+  implicit def FK2[A]: Bifunctor[K2[A, ?, ?]] = new Bifunctor[K2[A, ?, ?]] {
+    def bimap[A1, B1, A2, B2](
+        ka: K2[A, A1, A2])(f: A1 => B1, g: A2 => B2): K2[A, B1, B2] = K2(ka.unK)
   }
 
-/*
+  /*
       -- | Bifunctor constante 1
 
       data U2 a r = U2
 
       instance Bifunctor U2  where
          bimap f g U2 = U2
-*/
+   */
 
-  case class U2[A,R]() 
+  case class U2[A, T]()
 
-  implicit def FU2 =new Bifunctor[U2]{
-    def bimap[A1,B1,A2,B2](ua: U2[A1,A2])(f: A1 => B1, g: A2 => B2):U2[B1,B2]=U2()
+  implicit def FU2: Bifunctor[U2] = new Bifunctor[U2] {
+    def bimap[A1, B1, A2, B2](ua: U2[A1, A2])(f: A1 => B1,
+                                              g: A2 => B2): U2[B1, B2] = U2()
   }
 
-/*
+  /*
           -- | Primera proyeccion (ocurrencia parametro)
 
          data Par a r = Par a
@@ -66,28 +66,32 @@ object Bifunctors {
             bimap f g (Par x) = Par (f x)
 
 
- */
-  case class Par[A,R](unPar:A) 
+   */
+  case class Par[A, T](unPar: A)
 
-  implicit def FPar =new Bifunctor[Par]{
-    def bimap[A1,B1,A2,B2](para:Par[A1,A2])(f: A1 => B1, g: A2 => B2):Par[B1,B2]=Par(f(para.unPar))
+  implicit def FPar: Bifunctor[Par] = new Bifunctor[Par] {
+    def bimap[A1, B1, A2, B2](para: Par[A1, A2])(f: A1 => B1,
+                                                 g: A2 => B2): Par[B1, B2] =
+      Par(f(para.unPar))
   }
 
-/*
+  /*
    -- | Segunda proyeccion (ocurrencia posicion recursiva)
 
             data Rec a r = Rec r
 
             instance Bifunctor Rec where
                bimap f g (Rec r) = Rec (g r)
-*/
-  case class Rec[A,R](unRec:R) 
+   */
+  case class Rec[A, T](unRec: T)
 
-  implicit def FRec =new Bifunctor[Rec]{
-    def bimap[A1,B1,A2,B2](reca:Rec[A1,A2])(f: A1 => B1, g: A2 => B2):Rec[B1,B2]=Rec(g(reca.unRec))
+  implicit def FRec: Bifunctor[Rec] = new Bifunctor[Rec] {
+    def bimap[A1, B1, A2, B2](reca: Rec[A1, A2])(f: A1 => B1,
+                                                 g: A2 => B2): Rec[B1, B2] =
+      Rec(g(reca.unRec))
   }
 
-/*
+  /*
   -- | Suma de bifunctores
 
                data (f :++: g) a r = LL (f a r) | RR (g a r)
@@ -95,34 +99,45 @@ object Bifunctors {
                instance (Bifunctor t, Bifunctor u) => Bifunctor (t :++: u) where
                   bimap f g (LL x) = LL (bimap f g x)
                      bimap f g (RR y) = RR (bimap f g y)
-*/
+   */
 
-  trait :++:[F,G] 
-  case class LL[F,G](f:F) extends :++:[F,G]
-  case class RR[F,G](g:G) extends :++:[F,G]
+  trait :++:[F, G]
+  case class LL[F, G](f: F) extends :++:[F, G]
+  case class RR[F, G](g: G) extends :++:[F, G]
 
-implicit def bifplus[F[_,_] , G[_,_] ](implicit ff:Bifunctor[F], fg:Bifunctor[G])=new Bifunctor[Lambda[(A,R) => F[A,R]:++:G[A,R]]]{
-  def bimap[A,B,A2,B2](fga:F[A,A2]:++:G[A,A2])(f: A => B, g:A2=>B2):F[B,B2]:++:G[B,B2]= fga match {
-    case LL(lf) => LL(ff.bimap(lf)(f,g))
-    case RR(rg) => RR(fg.bimap(rg)(f,g))
-  }
-}
+  implicit def bifplus[F[_, _], G[_, _]](
+      implicit ff: Bifunctor[F],
+      fg: Bifunctor[G]): Bifunctor[Lambda[(A, R) => F[A, R] :++: G[A, R]]] =
+    new Bifunctor[Lambda[(A, R) => F[A, R] :++: G[A, R]]] {
+      def bimap[A, B, A2, B2](fga: F[A, A2] :++: G[A, A2])(
+          f: A => B,
+          g: A2 => B2): F[B, B2] :++: G[B, B2] = fga match {
+        case LL(lf) => LL(ff.bimap(lf)(f, g))
+        case RR(rg) => RR(fg.bimap(rg)(f, g))
+      }
+    }
 
-/*
+  /*
 implicit def bifplus[F[_,_], G[_,_]](implicit ff:Bifunctor[F], fg:Bifunctor[G])=new Bifunctor[BiFComb[F,G,:++:]#abs]{
   def bimap[A,B,A2,B2](fga:F[A,A2]:++:G[A,A2])(f: A => B, g:A2=>B2):F[B,B2]:++:G[B,B2]= fga match {
     case LL(lf) => LL(ff.bimap(lf)(f,g))
     case RR(rg) => RR(fg.bimap(rg)(f,g))
   }
 }
-*/
+   */
 
-case class :**:[F,G](f:F,g:G)  
+  case class :**:[F, G](f: F, g: G)
 
-implicit def bifstar[F[_,_], G[_,_]](implicit ff:Bifunctor[F], fg:Bifunctor[G])=new Bifunctor[Lambda[(A,R) => F[A,R]:**:G[A,R]]]{
-  def bimap[A,B,A2,B2](fga:F[A,A2]:**:G[A,A2])(f: A => B,g:A2=>B2):F[B,B2]:**:G[B,B2]= :**:(ff.bimap(fga.f)(f,g),fg.bimap(fga.g)(f,g))
-}
-/*
+  implicit def bifstar[F[_, _], G[_, _]](
+      implicit ff: Bifunctor[F],
+      fg: Bifunctor[G]): Bifunctor[Lambda[(A, R) => F[A, R] :**: G[A, R]]] =
+    new Bifunctor[Lambda[(A, R) => F[A, R] :**: G[A, R]]] {
+      def bimap[A, B, A2, B2](fga: F[A, A2] :**: G[A, A2])(
+          f: A => B,
+          g: A2 => B2): F[B, B2] :**: G[B, B2] =
+        :**:(ff.bimap(fga.f)(f, g), fg.bimap(fga.g)(f, g))
+    }
+  /*
 -- | Functor de tipo compuesto con bifunctor
 
 data (d :@@: f) a r = Comp2 {unComp2 :: d (f a r)}
@@ -131,12 +146,16 @@ instance (Regular2 d, Bifunctor (PF2 d), Bifunctor f) => Bifunctor (d :@@: f) wh
    bimap f g x = Comp2 $ pmap (bimap f g) $ unComp2 x
 
 
-*/
+   */
 
-  case class :@@:[D[_],F](unComp2:D[F], rd:Regular2[D])
+  case class :@@:[D[_], F](unComp2: D[F], rd: Regular2[D])
 
-  implicit def fbicomp[F[_,_],D[_]](implicit ff:Bifunctor[F])=new Bifunctor[Lambda[(A,R) => D:@@:F[A,R]]]{
-    def bimap[A,B,A2,B2](fga:D:@@:F[A,A2])(f: A => B,g: A2=>B2):D:@@:F[B,B2]= :@@:(pmap(fga.unComp2)(ff.bimap(_)(f,g))(fga.rd),fga.rd)
-  }
+  implicit def fbicomp[F[_, _], D[_]](
+      implicit ff: Bifunctor[F]): Bifunctor[Lambda[(A, R) => D :@@: F[A, R]]] =
+    new Bifunctor[Lambda[(A, R) => D :@@: F[A, R]]] {
+      def bimap[A, B, A2, B2](
+          fga: D :@@: F[A, A2])(f: A => B, g: A2 => B2): D :@@: F[B, B2] =
+        :@@:(pmap(fga.unComp2)(ff.bimap(_)(f, g))(fga.rd), fga.rd)
+    }
 
 }
--- src/main/scala/reggen/Functors.scala
+++ src/main/scala/reggen/Functors.scala
@@ -6,45 +6,57 @@ trait Functor[F[_]] {
 
 object Functors {
 
-  case class K[A,R](unK:A)
+  sealed trait PFn
 
-  implicit def FK[A] =new Functor[K[A,?]]{
-    def fmap[A1, B1](ka: K[A,A1])(f: A1 => B1): K[A,B1]=K(ka.unK)
+  case class K[A, Z](unK: A) extends PFn
+
+  implicit def FK[A]: Functor[K[A, ?]] = new Functor[K[A, ?]] {
+    def fmap[A1, B1](ka: K[A, A1])(f: A1 => B1): K[A, B1] = K(ka.unK)
   }
 
-  case class U[Z]()
+  case class U[Z]() extends PFn
 
-  implicit def FU =new Functor[U]{
-    def fmap[A, B](ua: U[A])(f: A => B):U[B]=U()
+  implicit def FU: Functor[U] = new Functor[U] {
+    def fmap[A, B](ua: U[A])(f: A => B): U[B] = U()
   }
 
-  case class I[Z](unI:Z)
-  implicit def FId =new Functor[I]{
-    def fmap[A, B](ida: I[A])(f: A => B):I[B]=I(f(ida.unI))
+  case class I[Z](unI: Z) extends PFn
+  implicit def FId: Functor[I] = new Functor[I] {
+    def fmap[A, B](ida: I[A])(f: A => B): I[B] = I(f(ida.unI))
   }
 
-  trait :+:[F,G] 
+  trait :+:[F, G] extends PFn
 
-  case class L[F,G](f:F) extends :+:[F,G]
-  case class R[F,G](g:G) extends :+:[F,G]
+  case class L[F, G](f: F) extends :+:[F, G]
+  case class R[F, G](g: G) extends :+:[F, G]
 
-
-  implicit def fplus[F[_], G[_]](implicit ff:Functor[F], fg:Functor[G])=new Functor[Lambda[A=> F[A]:+:G[A]]]{
-    def fmap[A,B](fga:F[A]:+:G[A])(f: A => B):F[B]:+:G[B]= fga match {
-      case L(lf) => L(ff.fmap(lf)(f))
-      case R(rg) => R(fg.fmap(rg)(f))
+  implicit def fplus[F[_], G[_]](
+      implicit ff: Functor[F],
+      fg: Functor[G]): Functor[Lambda[A => F[A] :+: G[A]]] =
+    new Functor[Lambda[A => F[A] :+: G[A]]] {
+      def fmap[A, B](fga: F[A] :+: G[A])(f: A => B): F[B] :+: G[B] = fga match {
+        case L(lf) => L(ff.fmap(lf)(f))
+        case R(rg) => R(fg.fmap(rg)(f))
+      }
     }
-  }
 
-  case class :*:[F,G](f:F,g:G) 
+  case class :*:[F, G](f: F, g: G) extends PFn
 
-  implicit def fstar[F[_], G[_]](implicit ff:Functor[F], fg:Functor[G])=new Functor[Lambda[A=> F[A]:*:G[A]]]{
-    def fmap[A,B](fga:F[A]:*:G[A])(f: A => B):F[B]:*:G[B]= :*:(ff.fmap(fga.f)(f),fg.fmap(fga.g)(f))
-  }
+  implicit def fstar[F[_], G[_]](
+      implicit ff: Functor[F],
+      fg: Functor[G]): Functor[Lambda[A => F[A] :*: G[A]]] =
+    new Functor[Lambda[A => F[A] :*: G[A]]] {
+      def fmap[A, B](fga: F[A] :*: G[A])(f: A => B): F[B] :*: G[B] =
+        :*:(ff.fmap(fga.f)(f), fg.fmap(fga.g)(f))
+    }
 
-  case class :@:[F[_],G](unComp:F[G])  
+  case class :@:[F[_], G](unComp: F[G]) extends PFn
 
-  implicit def fcomp[F[_], G[_]](implicit ff:Functor[F], fg:Functor[G])=new Functor[Lambda[A=>F:@:G[A]]]{
-    def fmap[A,B](fga:F:@:G[A])(f: A => B):F:@:G[B]= :@:(ff.fmap(fga.unComp)(fg.fmap(_)(f)))
-  }
+  implicit def fcomp[F[_], G[_]](
+      implicit ff: Functor[F],
+      fg: Functor[G]): Functor[Lambda[A => F :@: G[A]]] =
+    new Functor[Lambda[A => F :@: G[A]]] {
+      def fmap[A, B](fga: F :@: G[A])(f: A => B): F :@: G[B] =
+        :@:(ff.fmap(fga.unComp)(fg.fmap(_)(f)))
+    }
 }
